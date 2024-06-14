58d8d6dc51d9b9cc14716f5e816bfcedf937da27
mani2071995 <ramakrishnay207@gmail.com>
2024-06-14 12:05:51 -0400

    Remediated code

--- databaseconnector.java
+++ databaseconnector.java
@@ -1,19 +1,8 @@
-// File: DatabaseConnector.java
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
-public class DatabaseConnector {
-    private static final String URL = "jdbc:mysql://localhost:3306/mydatabase";
-    private static final String USER = "root";
-    private static final String PASSWORD = "password"; // Vulnerability: Hardcoded credentials
+String url = "jdbc:mysql://localhost:3306/mydatabase?useSSL=truerequireSSL=true";
 
-    public Connection connect() {
-        try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+try (Connection conn = connectionPool.getConnection(url, getUserName(), getPassword()) {
+  // use connection 
+} catch (CustomDBException e) {
+  logger.error("Error connecting to database", e); 
 }
