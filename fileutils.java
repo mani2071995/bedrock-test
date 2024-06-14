63a23fa47ca449b5bed21d979071fcdcd7ca892c
mani2071995 <ramakrishnay207@gmail.com>
2024-06-14 12:05:53 -0400

    Remediated code

--- fileutils.java
+++ fileutils.java
@@ -1,19 +1,43 @@
+java
 // File: FileUtils.java
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileUtils {
+
     public static String readFile(String filename) throws IOException {
-        File file = new File(filename);
-        StringBuilder content = new StringBuilder();
-        try (FileReader reader = new FileReader(file)) {
+        
+        // Use try-with-resources to automatically close resources
+        try (FileReader reader = new FileReader(filename)) {
+            
+            StringBuilder content = new StringBuilder();
             char[] buffer = new char[1024];
-            int read;
-            while ((read = reader.read(buffer)) != -1) {
-                content.append(buffer, 0, read);
+            int length;
+            
+            // Check for EOF
+            while ((length = reader.read(buffer)) > 0) {
+                content.append(buffer, 0, length);
             }
+            
+            return content.toString();
+            
+        } catch (IOException e) {
+            // Handle exception
+            throw e; 
         }
-        return content.toString();
+        
     }
+    
+    // Alternative implementation using Files.readAllBytes
+    public static String readFileAlternative(String filename) throws IOException {
+       
+        byte[] bytes = Files.readAllBytes(Paths.get(filename));
+        return new String(bytes, StandardCharsets.UTF_8);
+        
+    }
+
 }
